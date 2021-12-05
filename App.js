e121f2dcaf7bc9ae14f15422e38153b4de2cbe83
Jieun Oh <0909oje@naver.com>
2021-12-05 11:01:23 +0900

    싸인까지는 성공

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useRef}from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import Signature from "react-native-signature-canvas";
+import SignatureScreen from "react-native-signature-canvas";
+
+import Sign from './components/Sign'
 
 export default function App() {
+  const handleOK = (signature) => {
+    console.log(signature);
+    onOK(signature); // Callback from Component props
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Sign onOk={handleOK}/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
